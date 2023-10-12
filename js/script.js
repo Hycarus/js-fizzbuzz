841adc8c6ba466eaa4a9cbda5c170eef1ace16cb
Hycarus <mirkomarasco08@gmail.com>
2023-10-12 09:48:54 +0200

    refactory

--- js/script.js
+++ js/script.js
@@ -1,37 +1,27 @@
 const wrapperEl = document.getElementById('wrapper');
-
-
 for(let c = 1; c <= 100; c++){
     let bgBox;
     const boxEl = document.createElement('div');
     if(c % 3 === 0 && c % 5 === 0){
         bgBox = 'fizz-buzz';
         const fizzBuzz = ('Fizz-Buzz');
-        boxEl.className = `box ${bgBox} d-flex justify-content-center align-items-center `;
         boxEl.innerHTML = fizzBuzz;
-        wrapperEl.append(boxEl);
         console.log(fizzBuzz);
     } else if(c % 3 === 0){
         bgBox = 'fizz';
         const fizz = ('Fizz');
-        boxEl.className = `box ${bgBox} d-flex justify-content-center align-items-center `;
         boxEl.innerHTML = fizz;
-        wrapperEl.append(boxEl);
         console.log(fizz);
     } else if(c % 5 === 0){
         bgBox = 'buzz';
         const buzz = ('Buzz');
-        boxEl.className = `box ${bgBox} d-flex justify-content-center align-items-center `;
         boxEl.innerHTML = buzz;
-        wrapperEl.append(boxEl);
         console.log(buzz);
     } else{
         bgBox = 'number';
-        boxEl.className = `box ${bgBox} d-flex justify-content-center align-items-center `;
-        boxEl.innerHTML = c * 1;
-        wrapperEl.append(boxEl);
+        boxEl.innerHTML = c;
         console.log(c);
     }
-    
-    
+    wrapperEl.append(boxEl);
+    boxEl.className = `box ${bgBox} d-flex justify-content-center align-items-center `;
 }
